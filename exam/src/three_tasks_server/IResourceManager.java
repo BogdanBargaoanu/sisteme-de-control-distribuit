eb0b1169dc30942f6e605c2377f62e424f44833e
BogdanBargaoanu <bn19bba@yahoo.com>
2025-01-22 18:29:41 +0200

    fix

--- exam/src/three_tasks_server/IResourceManager.java
+++ exam/src/three_tasks_server/IResourceManager.java
@@ -2,4 +2,6 @@ package three_tasks_server;
 
 public interface IResourceManager {
     Resource getResource();
+    void AllowTask(Task task);
+    void RevokeTask(Task task);
 }
--- exam/src/three_tasks_server/Task.java
+++ exam/src/three_tasks_server/Task.java
@@ -1,4 +1,30 @@
 package three_tasks_server;
 
+import java.util.List;
+
 public class Task implements ITask{
+    @Override
+    public void SendMessageForResource(Resource res) {
+
+    }
+
+    @Override
+    public Resource AquireResource(ResourceManager resourceManager) {
+        return null;
+    }
+
+    @Override
+    public List<Resource> AquireBothResources(ResourceManager resourceManager1, ResourceManager resourceManager2) {
+        return List.of();
+    }
+
+    @Override
+    public void ReleaseResource(ResourceManager resourceManager) {
+
+    }
+
+    @Override
+    public void ReleaseBothResources(ResourceManager resourceManager1, ResourceManager resourceManager2) {
+
+    }
 }
